efbdaa9d1797888b9af7e4acb262b7b12216cb81
grace <graceogwe@gmail.com>
2024-05-14 04:58:50 +0100

    reactApp

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App(){
+  return(
+    <div className="app">
+      <h1 style={{position:"relative",bottom:"100px",left:"200px",color:"dodgerblue"}}>This is just a test React Application</h1>
+    </div>
+  )
+}
